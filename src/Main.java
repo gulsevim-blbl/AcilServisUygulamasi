efbae17f33f91b5bbc9787bd4c7515796235be8d
Gül Sevim BÜLBÜL <gulsevimblbl@gmail.com>
2023-06-22 01:32:08 +0300

    ilk commit atıldı

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Acil Servis Uygulaması
+        
+        /*
+        Hastalar Acil Serviste şikayetlerine göre kuyrukta en önlere geçecek(PriorityQueue).
+        
+        Apandisit ----> En yüksek öncelik
+        Yanık -------> Orta Öncelik
+        Baş Ağrısı ---> En düşük öncelik
+        
+        */
+        Queue<Hasta> acilservis = new PriorityQueue<Hasta>();
+        
+        acilservis.offer( new Hasta("Murat Bey","Yanik"));
+        acilservis.offer( new Hasta("Okan Bey","Bas Agrisi"));
+        acilservis.offer(new Hasta("Elif Hanim","Apandisit"));
+        acilservis.offer( new Hasta("Oguz Bey","Yanik"));
+        acilservis.offer(new Hasta("Merve Hanim","Yanik"));
+        acilservis.offer( new Hasta("Gizem Hanim","Apandisit"));
+        
+        int i = 1;
+        
+        while(acilservis.isEmpty() != true) {
+            System.out.println("**************************************");
+            System.out.println(i + ".sirada");
+            System.out.println(acilservis.poll());
+            System.out.println("**************************************");
+            i++;
+            
+        }
+    }
+}   
